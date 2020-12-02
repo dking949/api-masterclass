2bf2a467970500adc6b484cca46d4f9de4a17857
Darren King <dking949@gmail.com>
2020-12-02 13:35:56 +1100

    Adding DB setup

---
+++ models/Bootcamp.js
@@ -0,0 +1,100 @@
+const mongoose = require('mongoose');
+
+const BootcampSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true,
+        trim: true,
+        maxlength: [50, 'Name cannot be more than 50 characters']
+    },
+    slug: String,
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        maxlength: [500, 'Description cannot be more than 500 characters']
+    },
+    website: {
+        type: String,
+        match: [
+            /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/,
+            'Please use a valid URL with HTTP or HTTPs'
+        ]
+    },
+    phone: {
+        type: String,
+        maxlength: [20, 'Phone number cannot be longer than 20 characters']
+    },
+    email: {
+        type: String,
+        match: [
+            /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+            'Please add a valid email'
+        ]
+    },
+    address: {
+        type: String,
+        requires: [true, 'Please add an address']
+    },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+            },
+        coordinates: {
+            type: [Number],
+            required: true,
+            index: '2dsphere'
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        zipcode: String,
+        country: String
+    },
+    careers: {
+        type: [String],
+        required: true,
+        enum: [
+            'Web Development',
+            'Mobile Development',
+            'UI/UX',
+            'Data Science',
+            'Business',
+            'Other',
+        ]
+    },
+    averageRating: {
+        type: Number,
+        min: [1, 'Rating must be at least 1'],
+        max: [10, 'Rating must not be more than 10']
+    },
+    averageCost: Number,
+    photo: {
+        type: String,
+        default: 'no-photo.jpg'
+    },
+    housing: {
+        type: Boolean,
+        default: false
+    },
+    jobAssisstance: {
+        type: Boolean,
+        default: false
+    },
+    jobGuarantee: {
+        type: Boolean,
+        default: false
+    },
+    acceptGi: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
